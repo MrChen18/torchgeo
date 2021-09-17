70297a5dda9829630110697a402537b63a44d4ba
Caleb Robinson <calebrob6@gmail.com>
2021-09-17 21:21:29 +0000

    Adding a UnionDataset

--- torchgeo/datasets/__init__.py
+++ torchgeo/datasets/__init__.py
@@ -30,6 +30,7 @@ from .eurosat import EuroSAT
 from .geo import (
     GeoDataset,
     RasterDataset,
+    UnionDataset
     VectorDataset,
     VisionClassificationDataset,
     VisionDataset,
@@ -133,6 +134,7 @@ __all__ = (
     # Base classes
     "GeoDataset",
     "RasterDataset",
+    "UnionDataset",
     "VectorDataset",
     "VisionDataset",
     "VisionClassificationDataset",
